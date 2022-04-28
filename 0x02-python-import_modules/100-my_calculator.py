9d2c339c8dd3630c29e29eed01d4743efdadec8d
MartinDeFrancesco <defrancescoma@gmail.com>
2022-04-28 10:52:39 -0700

    fixed pep8

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    from calculator_1 import add, sub, mul, div   
+    from calculator_1 import add, sub, mul, div
     if (len(argv) - 1) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif (argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/'):
+    if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
