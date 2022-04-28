0c5aba4540e3f086c8fedcd435a1f2cac36fc875
MartinDeFrancesco <defrancescoma@gmail.com>
2022-04-28 10:49:56 -0700

    calculator fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul, div
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    operators = ["+", "-", "*", "/"]
-    if argv_count is not 3:
+    from sys import argv
+    from calculator_1 import add, sub, mul, div   
+    if (len(argv) - 1) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.argv[2] not in operators:
+    elif (argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/'):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
