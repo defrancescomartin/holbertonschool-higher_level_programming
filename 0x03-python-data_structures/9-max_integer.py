653c8190001e127799c574a0cd9da368e61d722c
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-02 08:44:26 -0700

    fixed var name

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
 
     biggest = 0
     for i in my_list:
-        if i > bigger:
-            bigger = i
+        if i > biggest:
+            biggest = i
 
-    return bigger
+    return biggest
