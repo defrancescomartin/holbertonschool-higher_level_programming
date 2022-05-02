8915b6f8c37f707a3749576ff6e86a8dd2e9c108
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-02 07:57:22 -0700

    max int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+
+    biggest = 0
+    for i in my_list:
+        if i > bigger:
+            bigger = i
+
+    return bigger
