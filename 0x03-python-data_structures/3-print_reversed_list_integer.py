4f96aa5fdbf258329d6ec0463a953cb546a26087
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-02 08:37:23 -0700

    checker fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,5 @@ def print_reversed_list_integer(my_list=[]):
         print('', end='')
     else:
         my_list.reverse()
-        for i in my_list:
+        for i in range(len(my_list)):
             print(f"{my_list[i]}")
