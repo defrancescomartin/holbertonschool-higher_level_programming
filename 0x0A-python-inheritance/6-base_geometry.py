3bf7f1f337749bf3b7744fc55cb7921dbdf8b50a
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-30 12:25:14 -0700

    area function

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Creates an empty class BaseGeometry """
+
+
+class BaseGeometry:
+    """ Class BaseGeometry """
+
+    def area(self):
+        """ area function """
+        raise Exception('area() is not implemented')
