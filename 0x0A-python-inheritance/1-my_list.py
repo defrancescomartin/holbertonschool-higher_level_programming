7fa95343c1eb04732818eaf57323f35d9d636288
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-30 11:24:06 -0700

    fixing codestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Module MyList that inherits from list """
 
+
 class MyList(list):
-	""" MyList class is a sublcass of list """
+    """ MyList class is a sublcass of list """
 
     def print_sorted(self):
         """ Print the list in sorted ascending order """
