a7c31cbe7394853fa39c3363da499f4d7922422f
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-30 11:42:16 -0700

    3 is kind of class taske with codestyle right

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Checks if is kind of class """
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function that checks if the obj is an instance of a
+    specified class or if is an instance of a class
+    that inherited from the specified class
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
