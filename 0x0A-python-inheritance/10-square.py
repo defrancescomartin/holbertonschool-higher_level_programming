0a378d4c8f8c038b4fbcf94acccdf51c3cef4044
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-30 14:44:14 -0700

    pycodefix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Create a subclass Square that inherits from class Rectangle 
+Create a subclass Square that inherits from class Rectangle
 """
 Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     """
