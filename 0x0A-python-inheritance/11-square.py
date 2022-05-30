03f9e37219df655a5077f50cc601b4b825f01d23
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-30 13:05:47 -0700

    11 square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Create a subclass Square that inherits from class Rectangle 
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    Square - is a subclass of Rectangle
+    """
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """override __str__ of Rectangle"""
+        return f"[Square] {self.__size}/{self.__size}"
