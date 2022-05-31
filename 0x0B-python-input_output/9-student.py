6d53710ed5f9a2248a3eaa1a9fa46722178da667
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-31 14:56:50 -0700

    try

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,10 +6,13 @@ by first and last name and age
 
 
 class Student:
+    """ defines a student  """
     def __init__(self, first_name, last_name, age):
+    """ initializer  """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+    """ dictionary representation of Student """
         return self.__dict__
