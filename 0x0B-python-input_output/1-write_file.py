40e4255387a34cb9b03804d310c82c062f997ba2
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-31 06:15:36 -0700

    1 write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Function that returns the number of chars
+written into a string on a textfile
+"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(text)
