ceb935331f093e019bf0e8fac87a112d64dab866
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-31 14:39:37 -0700

    try

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,6 @@ written into a string on a textfile
 
 
 def write_file(filename="", text=""):
+    """ write a string into a textfile """
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
