60ab18a55e3148381e5fed7c092d82e19b6c8036
MartinDeFrancesco <defrancescoma@gmail.com>
2022-05-31 15:02:35 -0700

    try

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,12 +6,15 @@ by first and last name and age
 
 
 class Student:
+    """ defines a student """
     def __init__(self, first_name, last_name, age):
+        """ initializer """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """ retrieves a dictionary represent """
         if attrs is None:
             return self.__dict__
         dictionary = {}
@@ -21,5 +24,6 @@ class Student:
                 return dictionary
 
     def reload_from_json(self, json):
+        """ replace all attributes of the Studet """
         for key, value in json.items():
             setattr(self, key, value)
