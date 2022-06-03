46c413a9d46b13b1c77b185e82e373d0d708ada9
defrancescomartin <defrancescomartin@LAPTOP-RFLPL88P.localdomain>
2022-06-03 16:40:05 -0300

    base task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" Define class Base """
+import json
+from os import path
+
+
+class Base(object)
+    """" Define class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initialize constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects       
