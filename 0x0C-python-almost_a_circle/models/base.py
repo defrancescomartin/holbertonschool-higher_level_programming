8eff080b2c41dde08f153fb25bf577e79a13bd08
defrancescomartin <defrancescomartin@LAPTOP-RFLPL88P.localdomain>
2022-06-04 14:28:15 -0300

    tasks 15 & 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,3 +15,20 @@ class Base(object):
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dict):
+        """ returns the JSON representation of list_dict """
+        if list_dict is None or len(list_dict) == 0:
+            return "[]"
+        return json.dumps(list_dict)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a txtfile"""
+        with open(cls.__name__ + '.json', 'w', encoding='utf-8') as f:
+            if list_objs is None:
+                f.write('[]')
+            else:
+                f.write(cls.to_json_string([o.to_dictionary()
+                        for o in list_objs]))
