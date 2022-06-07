3474c66f7e72a3714729341e68c33eb74bb1c8ff
defrancescomartin <defrancescoma@gmail.com>
2022-06-07 10:18:16 -0300

    fixed file

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -63,54 +63,6 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle('Monty', 'Python')
 
-    def test_width(self):
-        """
-        Test setting attribute width
-        """
-        r1 = Rectangle(1, 2)
-        r2 = Rectangle(3, 4)
-
-        self.assertEqual(r1.width, 1)
-        self.assertEqual(r2.width, 3)
-
-    def test_height(self):
-        """
-        Test setting attribute height
-        """
-        r1 = Rectangle(1, 2)
-        r2 = Rectangle(3, 4)
-
-        self.assertEqual(r1.height, 2)
-        self.assertEqual(r2.height, 4)
-
-    def test_no_arguments(self):
-        """
-        Test when attributes width and height aren't passed
-        """
-        with self.assertRaises(TypeError):
-            r1 = Rectangle()
-
-    def test_with_width_and_height(self):
-        """
-        Test when attributes width and height are passed
-        """
-        Base._Base__nb_objects = 0
-
-        r1 = Rectangle(2, 3)
-        r2 = Rectangle(4, 5)
-
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r1.width, 2)
-        self.assertEqual(r1.height, 3)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
-
-        self.assertEqual(r2.id, 2)
-        self.assertEqual(r2.width, 4)
-        self.assertEqual(r2.height, 5)
-        self.assertEqual(r2.x, 0)
-        self.assertEqual(r2.y, 0)
-
     def test_type_param(self):
         """
         Test different types of parameters
