33caf525b1341efa165d280ce8c8d7a0c0d4baaa
defrancescomartin <defrancescoma@gmail.com>
2022-08-01 16:18:55 -0300

    pushing all mandatory tasks testes w/ permissions and semistandart

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVarC = 'C is fun';
+const myVarP = 'Python is cool';
+const myVarJS = 'JavaScript is amazing';
+console.log(myVarC);
+console.log(myVarP);
+console.log(myVarJS);
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const n = parseInt(process.argv[2]);
+
+if (n) {
+  console.log('My number: ' + n);
+} else {
+  console.log('Not a number');
+}
