60a4c2b7771eaff814f10c9750516e61f917fea5
defrancescomartin <defrancescoma@gmail.com>
2022-08-07 13:37:46 -0700

    prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,9 +10,9 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
 
-    MY_USER = sys.argv[1]
-    MY_PASS = sys.argv[2]
-    MY_DB = sys.argv[3]
+    MY_USER = argv[1]
+    MY_PASS = argv[2]
+    MY_DB = argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(MY_USER, MY_PASS, MY_DB))
