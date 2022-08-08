375a5c031fa643931f38654e563cdce8808ae542
defrancescomartin <defrancescoma@gmail.com>
2022-08-08 14:19:45 -0300

    fixing style

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""script that prints the first State object 
+"""script that prints the first State object
 from the database hbtn_0e_6_usa"""
 
 from sqlalchemy import create_engine
